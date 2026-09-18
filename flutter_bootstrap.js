4309cb6445730c6557e296d3f618902569f6dda5
sahil <sahilshahcse@gmail.com>
2026-09-18 22:50:11 +0530

    Akhada console, hosted build 2026-09-18 - the board is charts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "195707404" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2412956518" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
