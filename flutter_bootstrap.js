1221aa77041ef3a66e4e22060142bd7fbde5d283
sahil <sahilshahcse@gmail.com>
2026-09-19 09:53:49 +0530

    Akhada console, hosted build 2026-09-19 - the door code, no face

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2412956518" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1132623225" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
